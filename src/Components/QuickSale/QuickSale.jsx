42340b53b4c2b15b04768bcb4b2eedee103f0dcc
ajijul-islam-dev <ajijulislam5938@gmail.com>
2024-08-18 12:01:54 +0600

    Quick sale section created

--- src/Components/QuickSale/QuickSale.jsx
+++ src/Components/QuickSale/QuickSale.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const QuickSale = () => {
     return (
-        <div>
-            Quick sale
+        <div className='grid grid-rows-2 md:grid-rows-1 grid-cols-2 md:grid-cols-3 gap-3 border'>
+            <Link className=''> <img className='rounded-md w-full h-full' src="images/category/flash_sale.jpg" alt="" /></Link>
+            <Link className='row-span-2 md:row-span-1 h-full'> <img className='rounded-md hidden md:block' src="images/category/group_purchase.jpg" alt="" /><img className='rounded-md  md:hidden' src="images/category/group_vertical.jpg" alt="" /></Link>
+            <Link className=''><img className='rounded-md w-full h-full' src="images/category/history.jpg" alt="" /></Link>
         </div>
     );
 };
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,19 +1,24 @@
 import React from "react";
 import SideBar from "../../Components/Sidebar/Sidebar";
 import BannerSlider from "../../Components/BannerSlider/BannerSlider";
+import QuickSale from "../../Components/QuickSale/QuickSale";
 
 function Home() {
   return (
     <div className="">
-      <div className="grid grid-cols-1 md:grid-cols-10 border w-full overflow-x-hidden">
+      <div className="grid grid-cols-1 md:grid-cols-10 w-full overflow-x-hidden">
         <div className="md:col-span-2 hidden md:block">
           <SideBar />
         </div>
         <div className="md:col-span-8 ">
-          <BannerSlider />
+          <div className="">
+            <BannerSlider />
+          </div>
+          <div className="my-10">
+            <QuickSale />
+          </div>
         </div>
       </div>
-      
     </div>
   );
 }
